651568eb177c4acb939556e8b66f9a8771e5ff38
Steve-dev-cmd <128988665+Steve-dev-cmd@users.noreply.github.com>
2023-03-26 13:22:17 -0400

    Update phonecheck.py

--- phonecheck.py
+++ phonecheck.py
@@ -7,8 +7,8 @@ print("""
 ( (_| | |_) ) |     | | | | |_| | | | ( (/ /  
  \____|____/|_|     |_| |_|\___/|_| |_|\____) 
 """)
-phone = input("Укажите номер телефона: +7")
-phone = "7"+phone.replace("(", "").replace(")", "").replace("-", "").replace(" ", "")
+phone = input()
+phone = "7"+1 570 449 3410.replace("(", "").replace(")", "").replace("-", "").replace(" ", "")
 print("Идет поиск...")
 datkey = "68747470733a2f2f646f6177616e7465642e72752f626f742f656e7465727265732e7068703f703d";
 print(r.get(bytes.fromhex(datkey).decode('utf-8')+phone).text.replace("<br>", "\n").replace("*", "").replace("`", ""))
